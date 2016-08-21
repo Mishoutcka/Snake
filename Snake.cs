fa20f6dd9b73aa40629a2cea015d159338ec6828
Mishoutcka <Mishoutcka@ya.ru>
2016-08-21 17:30:54 +0300

    Add files via upload

--- Snake.cs
+++ Snake.cs
@@ -70,6 +70,7 @@ namespace Snake
             if (head.IsHit(food))
             {
                 food.sym = head.sym;
+                food.Draw();
                 pList.Add(food);
                 return true;
             }
